fbdb0d8d4f69583119da402c5eaba7e91cc79796
Xiangmu Gao <ethangao06@gmail.com>
2026-09-16 13:59:14 -0400

    add our motto

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,6 @@ fn main() {
     println!("  (nobody has signed on yet)");
 
     println!();
-    println!("Motto: {}", MOTTO);
+    println!("Motto: {}", motto);
     println!("Report any problems to whoever merged last.");
 }
